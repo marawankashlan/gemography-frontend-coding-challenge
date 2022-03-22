528c661946eb53164e449e643610b1ec9d6fdb03
marawankashlan <46195409+marawankashlan@users.noreply.github.com>
2022-03-22 13:53:42 +0200

    main component added
    main component added to the project

---
+++ gemography-frontend-coding-challenge/src/components/main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+class main extends React.Component {
+    constructor() {
+      super();
+      this.state = {color: "red"};
+    }
+    render() {
+            return (
+                <div className="App">
+                       <h1>test</h1>
+                </div>
+          );
+    }
+  }
+  export default main;
