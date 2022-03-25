f97ede4f96791627b515eab2c480c06178341667
marawankashlan <46195409+marawankashlan@users.noreply.github.com>
2022-03-25 18:54:02 +0200

    dependencies installed
    superagent , and lodash.debounce installed to help in the infinite scroll functionality

--- gemography-frontend-coding-challenge/src/components/main.js
+++ gemography-frontend-coding-challenge/src/components/main.js
@@ -15,24 +15,26 @@ class main extends React.Component {
     axios.get(`https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc`)
       .then(res => {
         const Response_arr = res.data.items;
-        //console.log("arr",JSON.stringify(res.data))
         this.setState({ arr:Response_arr });
       })
     }
 
     render() {
         var array=[]
-        var today = new Date();
         
         this.state.arr.forEach(element => {
             let ndate=new Date(element.created_at)
             let today = new Date()
+            let temp=''
+            if(element.description){
+              temp=element.description.slice(0,3)
+            }
             let Difference_In_Time = today.getTime() - ndate.getTime();
             // To calculate the no. of days between two dates
             let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
             let days = Math.trunc(Difference_In_Days)
 
-            array.push(<div>
+            array.push(<div className="card">
                 <br/>
                 <br/>
                 <div className="media">
@@ -40,18 +42,12 @@ class main extends React.Component {
                         <div >
                               <div><h2 className="repo">{element.name}</h2></div>
                               <div><p className="desc">{element.description}</p></div>
-                        <div>
-                        <div>
-                            <div >
+                              <div >
                                 <h5 className="rest"><i className="box"><AiFillStar className="icon"/>  Stars: {element.stargazers_count}</i> <i className="box"><BsRecordCircle /> Issues: {element.open_issues_count} </i> <small>Submitted {days} days ago by {element.owner.login}</small></h5>
                             </div>
                         </div>
-                    </div>
-                    
                 </div>
-                </div>
-                
-                <hr className="hr"/>
+                <br/>
             </div>)
         });
         
