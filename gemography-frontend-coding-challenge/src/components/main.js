cddba6da313523d50e0fe6d9088c468aabf04a92
marawankashlan <46195409+marawankashlan@users.noreply.github.com>
2022-03-25 19:28:34 +0200

    loadpage function added
    a function that loads the repos of the next page is added

--- gemography-frontend-coding-challenge/src/components/main.js
+++ gemography-frontend-coding-challenge/src/components/main.js
@@ -2,6 +2,7 @@ import React from "react"
 import axios from 'axios';
 import { AiFillStar } from 'react-icons/ai';
 import { BsRecordCircle } from 'react-icons/bs';
+import debounce from 'lodash.debounce';
 import "./styles.css";
 
 class main extends React.Component {
@@ -9,14 +10,50 @@ class main extends React.Component {
       super();
       this.state = {
         arr:[],
+        error: false,
+        hasMore: true,
+        isLoading: false,
+        page:1
       };
     }
     componentDidMount(){
-    axios.get(`https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc`)
-      .then(res => {
-        const Response_arr = res.data.items;
-        this.setState({ arr:Response_arr });
-      })
+      this.loadPage()
+    }
+
+    loadPage = () =>{
+      if(this.state.page==1){
+            this.setState({ isLoading: true },()=> {
+              axios.get(`https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc`)
+              .then(res => {
+                let pageTemp=this.state.page;
+                pageTemp++;
+                const Response_arr = res.data.items;
+                this.setState({ arr:Response_arr,isLoading: false,page:pageTemp });
+              })
+              .catch((err)=>{
+                this.setState({error: err.message,
+                  isLoading: false});
+              });
+            }
+          )
+      }
+      else{
+        let pageTemp=this.state.page;
+        this.setState({ isLoading: true },()=> {
+          axios.get(`https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=`+pageTemp)
+          .then(res => {
+            pageTemp++;
+            const Response_arr = res.data.items;
+            this.setState({ arr:Response_arr,isLoading: false,page:pageTemp });
+          })
+          .catch((err)=>{
+            this.setState({error: err.message,
+              isLoading: false});
+          });
+        }
+      )
+      }
+      
     }
 
     render() {
